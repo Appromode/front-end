ef54671622d629d7b656f247cd94d77ddacbf119
Alexander Watts <acwatts.dev@gmail.com>
2021-11-08 15:18:19 +0000

    Added react bootstrap components to default page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
+import { Container, Row } from 'react-bootstrap';
 
 const Index:FC = () => (
-  <div>Marking System</div>
+  <Container>
+    <Row>
+      <div>Marking System</div>
+    </Row>
+  </Container>
 );
 
 export default Index;
