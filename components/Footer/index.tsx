25aeafe0ec0cbfcd9214a23b93e58fd45638e8e5
Faraz <farazl786@hotmail.com>
2021-11-19 11:25:41 +0000

    Created footer component with styles

---
+++ components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  Container,
+} from 'react-bootstrap';
+import styles from './styles.module.scss';
+
+const Footer = () => (
+  <Container id={styles.Footer}>
+    Project registry
+  </Container>
+);
+
+export default Footer;
