897c3554684a74a52b69e5a8efa2a8392fe6dc86
Faraz <farazl786@hotmail.com>
2021-11-26 11:34:49 +0000

    Fixed footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import {
-  Container,
-} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import styles from './styles.module.scss';
 
 const Footer = () => (
-  <Container id={styles.footer}>
-    <p> Project registry </p>
-  </Container>
+  <Row id={styles.footer}>
+    <Col id={styles.footerText}> project-registry.org </Col>
+  </Row>
 );
 
 export default Footer;
