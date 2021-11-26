594e0a508a108153326bc4258c8aee03df49edca
Brad <bss25@kent.ac.uk>
2021-11-26 11:13:21 +0000

    Added some form validation

--- components/RegistrationForm/index.tsx
+++ components/RegistrationForm/index.tsx
@@ -5,13 +5,22 @@ import {
   FormControl,
   FormGroup,
   FormLabel,
+  FormSelect,
+  InputGroup,
 } from 'react-bootstrap';
 import {
   Formik,
   Form,
 } from 'formik';
+import * as Yup from 'yup';
 import Tags from '../Tags';
 import styles from './styles.module.scss';
+
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string().required('Required'),
+  lastName: Yup.string().required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+});
 
 const RegistrationForm:FC = () => (
   <>
@@ -19,26 +28,39 @@ const RegistrationForm:FC = () => (
     <div className={styles.centreConsole}>
       <p id={styles.title1}>Student Registration</p>
       <Formik
+        validationSchema={SignupSchema}
+        onSubmit={console.log}
         initialValues={{
+          role: '',
           firstName: '',
           lastName: '',
           kentID: 0,
           email: '',
           // skills: skillslist,
         }}
-        onSubmit={(values, actions) => {
-          console.log({ values, actions });
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }}
       >
         <div className={styles.textbox}>
           <Form>
             <Row>
               <Col lg={{ span: 10, offset: 1 }}>
+                <FormGroup controlId="role">
+                  <FormLabel>Role</FormLabel>
+                  <FormSelect>
+                    <option>Select a role</option>
+                    <option>Student</option>
+                    <option>Project Supervisor</option>
+                    <option>Module Conevenor</option>
+                    <option>Admin</option>
+                  </FormSelect>
+                </FormGroup>
+              </Col>
+            </Row>
+            <div className={styles.formspacer} />
+            <Row>
+              <Col lg={{ span: 10, offset: 1 }}>
                 <FormGroup controlId="firstname">
                   <FormLabel>First Name</FormLabel>
-                  <FormControl type="text" placeholder="First Name" />
+                  <FormControl name="firstName" type="text" placeholder="First Name" />
                 </FormGroup>
               </Col>
             </Row>
@@ -65,7 +87,10 @@ const RegistrationForm:FC = () => (
               <Col lg={{ span: 10, offset: 1 }}>
                 <FormGroup controlId="email">
                   <FormLabel>Kent Email</FormLabel>
-                  <FormControl type="email" placeholder="example@kent.ac.uk" />
+                  <InputGroup>
+                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                    <FormControl type="email" placeholder="example@kent.ac.uk" />
+                  </InputGroup>
                 </FormGroup>
               </Col>
             </Row>
