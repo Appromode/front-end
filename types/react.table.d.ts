5cdeac7c06aac41e3630b3b027822ed6c049c7f1
acwatts <acwatts.dev@gmail.com>
2021-11-28 00:01:58 +0000

    Created types to extend react table preventing errors

---
+++ types/react.table.d.ts
@@ -0,0 +1,35 @@
+import {
+  Row,
+  IdType,
+  TableOptions,
+  UseTableInstanceProps,
+} from 'react-table';
+
+declare module 'react-table' {
+  type Rows = Row[];
+
+  interface GlobalFilter {
+    globalFilter: string
+  }
+
+  interface PreGlobalFilteredRows {
+    preGlobalFilteredRows: Array<Row>;
+  }
+
+  interface SetGlobalFilter {
+    setGlobalFilter: (filterValue) => void;
+  }
+
+  interface Filter extends GlobalFilter, PreGlobalFilteredRows, SetGlobalFilter { }
+
+  interface TableInstance<D extends object = {}>
+    extends Omit<TableOptions<D>,
+    'columns' | 'pageCount'>,
+    UseTableInstanceProps<D>,
+    PreGlobalFilteredRows,
+    SetGlobalFilter { }
+
+  interface TableState<D extends object = {}> extends GlobalFilter {
+    hiddenColumns?: Array<IdType<D>> | undefined;
+  }
+}
