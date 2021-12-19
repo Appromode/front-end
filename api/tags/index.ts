931198f33ec1f612428273b35efd92e31e713cf2
acwatts <acwatts.dev@gmail.com>
2021-12-19 16:55:01 +0000

    Updated tag methods call to api routes

--- api/tags/index.ts
+++ api/tags/index.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr';
 import fetcher from '../../utils/fetcher';
 
 export const getTags = () => {
-  const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_API_ROUTE}/api/Tag`, fetcher);
+  const { data, error } = useSWR('/api/Tag', fetcher);
 
   return Object.freeze({
     tags: data,
@@ -11,7 +11,7 @@ export const getTags = () => {
 };
 
 export const getTag = ({ id }: { id: number }) => {
-  const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_API_ROUTE}/api/Tag/${id}`, fetcher);
+  const { data, error } = useSWR(`/api/Tag/${id}`, fetcher);
 
   return Object.freeze({
     tag: data,
