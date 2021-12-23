713b07c525dcfa477007b088a3f67600570e402c
acwatts <acwatts.dev@gmail.com>
2021-12-23 12:47:39 +0000

    Added tag columns and used get tags function to get tag data from api

--- components/GroupRegistrationForm/index.tsx
+++ components/GroupRegistrationForm/index.tsx
@@ -11,49 +11,65 @@ import {
 } from 'react-bootstrap';
 import Searcher from '../Searcher';
 import styles from './styles.module.scss';
+import { getTags } from '../../api/tags';
 
-const GroupRegistrationForm: FC = () => (
-  <Container>
-    <h2 className={styles.formHeading}>Group Registration</h2>
-    <Formik
-      initialValues={{
-        groupName: '',
-        groupDescription: '',
-        groupMembers: [],
-        files: [],
-        isGroupVisible: false,
-      }}
-      onSubmit={() => undefined}
-      validationSchema={
-        object({
-          groupName: string().min(5).max(30).required(),
-          groupDescription: string().min(10).max(100).required(),
-          groupMembers: array().min(2).max(4).required(),
-          groupFiles: array(),
-        })
-      }
-    >
-      {({ touched, errors }) => (
-        <Form>
-          <FormGroup controlId="groupName" className={styles.formGroup}>
-            {(touched.groupName && errors.groupName) ? <Alert>{errors.groupName}</Alert> : ''}
-            <FormLabel>Group Name</FormLabel>
-            <Field type="text" name="groupName" as={FormControl} placeholder="Amazing group" />
-          </FormGroup>
+const GroupRegistrationForm: FC = () => {
+  const tagColumns = [
+    {
+      Header: 'tagId',
+      accessor: 'tagId',
+    },
+    {
+      Header: 'tagName',
+      accessor: 'tagName',
+    },
+  ];
 
-          <FormGroup controlId="groupDescription" className={styles.formGroup}>
-            {(touched.groupDescription && errors.groupDescription) ? <Alert>{errors.groupDescription}</Alert> : ''}
-            <FormLabel>Group Description</FormLabel>
-            <Field type="text" name="groupDescription" as={FormControl} placeholder="A truly amazing group" />
-          </FormGroup>
+  const { tags } = getTags();
 
-          <Searcher>Searcher</Searcher>
+  return (
+    <Container>
+      <h2 className={styles.formHeading}>Group Registration</h2>
+      <Formik
+        initialValues={{
+          groupName: '',
+          groupDescription: '',
+          groupMembers: [],
+          files: [],
+          isGroupVisible: false,
+        }}
+        onSubmit={() => undefined}
+        validationSchema={
+          object({
+            groupName: string().min(5).max(30).required(),
+            groupDescription: string().min(10).max(100).required(),
+            groupMembers: array().min(2).max(4).required(),
+            groupFiles: array(),
+          })
+        }
+      >
+        {({ touched, errors }) => (
+          <Form>
+            <FormGroup controlId="groupName" className={styles.formGroup}>
+              {(touched.groupName && errors.groupName) ? <Alert>{errors.groupName}</Alert> : ''}
+              <FormLabel>Group Name</FormLabel>
+              <Field type="text" name="groupName" as={FormControl} placeholder="Amazing group" />
+            </FormGroup>
 
-          <Button type="submit">Create Group</Button>
-        </Form>
-      )}
-    </Formik>
-  </Container>
-);
+            <FormGroup controlId="groupDescription" className={styles.formGroup}>
+              {(touched.groupDescription && errors.groupDescription) ? <Alert>{errors.groupDescription}</Alert> : ''}
+              <FormLabel>Group Description</FormLabel>
+              <Field type="text" name="groupDescription" as={FormControl} placeholder="A truly amazing group" />
+            </FormGroup>
+
+            <Searcher cols={tagColumns} allData={tags || []} />
+
+            <Button type="submit">Create Group</Button>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
+};
 
 export default GroupRegistrationForm;
