57a8e572f42be8c2d78536b5ef9021587e154ccf
acwatts <acwatts.dev@gmail.com>
2022-01-05 16:05:58 +0000

    Added new global styles to use tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 
