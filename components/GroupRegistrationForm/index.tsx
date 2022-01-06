618cdc818f478f0a535be150c9f8263514754753
acwatts <acwatts.dev@gmail.com>
2022-01-06 16:19:44 +0000

    Added user search component and removed unused data

--- components/GroupRegistrationForm/index.tsx
+++ components/GroupRegistrationForm/index.tsx
@@ -9,67 +9,54 @@ import {
   Button,
   Alert,
 } from 'react-bootstrap';
-import Searcher from '../Searcher';
 import styles from './styles.module.scss';
-import { getTags } from '../../api/tags';
+import UserSearch from '../UserSearch';
 
-const GroupRegistrationForm: FC = () => {
-  const tagColumns = [
-    {
-      Header: 'tagId',
-      accessor: 'tagId',
-    },
-    {
-      Header: 'tagName',
-      accessor: 'tagName',
-    },
-  ];
+const GroupRegistrationForm: FC = () => (
+  <Container>
+    <h2 className={styles.formHeading}>Group Registration</h2>
+    <Formik
+      initialValues={{
+        groupName: '',
+        groupDescription: '',
+        groupMembers: [],
+        files: [],
+        isGroupVisible: false,
+      }}
+      onSubmit={() => undefined}
+      validationSchema={
+        object({
+          groupName: string().min(5).max(30).required(),
+          groupDescription: string().min(10).max(100).required(),
+          groupMembers: array().min(2).max(4).required(),
+          groupFiles: array(),
+        })
+      }
+    >
+      {({ values, touched, errors }) => (
+        <Form>
+          <FormGroup controlId="groupName" className={styles.formGroup}>
+            {(touched.groupName && errors.groupName) ? <Alert>{errors.groupName}</Alert> : ''}
+            <FormLabel>Group Name</FormLabel>
+            <Field type="text" name="groupName" as={FormControl} placeholder="Amazing group" />
+          </FormGroup>
 
-  const { tags } = getTags();
+          <FormGroup controlId="groupDescription" className={styles.formGroup}>
+            {(touched.groupDescription && errors.groupDescription) ? <Alert>{errors.groupDescription}</Alert> : ''}
+            <FormLabel>Group Description</FormLabel>
+            <Field type="text" name="groupDescription" as={FormControl} placeholder="A truly amazing group" />
+          </FormGroup>
 
-  return (
-    <Container>
-      <h2 className={styles.formHeading}>Group Registration</h2>
-      <Formik
-        initialValues={{
-          groupName: '',
-          groupDescription: '',
-          groupMembers: [],
-          files: [],
-          isGroupVisible: false,
-        }}
-        onSubmit={() => undefined}
-        validationSchema={
-          object({
-            groupName: string().min(5).max(30).required(),
-            groupDescription: string().min(10).max(100).required(),
-            groupMembers: array().min(2).max(4).required(),
-            groupFiles: array(),
-          })
-        }
-      >
-        {({ touched, errors }) => (
-          <Form>
-            <FormGroup controlId="groupName" className={styles.formGroup}>
-              {(touched.groupName && errors.groupName) ? <Alert>{errors.groupName}</Alert> : ''}
-              <FormLabel>Group Name</FormLabel>
-              <Field type="text" name="groupName" as={FormControl} placeholder="Amazing group" />
-            </FormGroup>
+          {/* <Searcher cols={tagColumns} allData={tags || []} /> */}
+          {console.log(values.groupMembers)}
 
-            <FormGroup controlId="groupDescription" className={styles.formGroup}>
-              {(touched.groupDescription && errors.groupDescription) ? <Alert>{errors.groupDescription}</Alert> : ''}
-              <FormLabel>Group Description</FormLabel>
-              <Field type="text" name="groupDescription" as={FormControl} placeholder="A truly amazing group" />
-            </FormGroup>
+          <UserSearch />
 
-            <Searcher cols={tagColumns} allData={tags || []} />
-
-            <Button type="submit">Create Group</Button>
-          </Form>
-        )}
-      </Formik>
-    </Container>
-  );
-};
+          <Button type="submit">Create Group</Button>
+        </Form>
+      )}
+    </Formik>
+  </Container>
+);
 
 export default GroupRegistrationForm;
