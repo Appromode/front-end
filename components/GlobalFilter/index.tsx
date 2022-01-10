aaa81c33744b9fd542c85596fa18e23603e263ad
acwatts <acwatts.dev@gmail.com>
2022-01-10 22:55:40 +0000

    Added new label prop and type and updated form field with search icon and tailwind

--- components/GlobalFilter/index.tsx
+++ components/GlobalFilter/index.tsx
@@ -1,19 +1,42 @@
 import React, { FC, ChangeEvent } from 'react';
 import { Filter } from 'react-table';
+import Image from 'next/image';
 
-const GlobalFilter: FC<Filter> = ({ preGlobalFilteredRows, globalFilter, setGlobalFilter }) => {
+const GlobalFilter: FC<Filter & { label: string }> = ({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+  label,
+}) => {
   const rowCount = preGlobalFilteredRows.length;
 
   const onChange = (event: ChangeEvent<HTMLInputElement>) => setGlobalFilter(event.target.value);
 
   return (
-    <input
-      className="p-2 w-full border-1 mb-4 rounded-md"
-      type="text"
-      value={globalFilter || ''}
-      onChange={onChange}
-      placeholder={`Search ${rowCount} rows`}
-    />
+    <label htmlFor="globalFilter" className="relative block mb-3">
+      <div className="mb-2">{label}</div>
+      <div className="relative">
+        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+          <Image
+            src="/search.svg"
+            layout="fixed"
+            alt="Search icon"
+            width={20}
+            height={20}
+            className="block"
+          />
+        </span>
+        <input
+          autoComplete="off"
+          id="globalFilter"
+          className="py-2 pl-10 pr-3 w-full border-1 border-gray-300 rounded-md"
+          type="text"
+          value={globalFilter || ''}
+          onChange={onChange}
+          placeholder={`Search ${rowCount} ${rowCount === 1 ? 'row' : 'rows'}`}
+        />
+      </div>
+    </label>
   );
 };
 
