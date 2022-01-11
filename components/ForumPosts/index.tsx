1a38d942d930f40d5c8a1e73df9b0305a953a1ae
Brad <bss25@kent.ac.uk>
2022-01-11 13:27:05 +0000

    Styled the forum post page

--- components/ForumPosts/index.tsx
+++ components/ForumPosts/index.tsx
@@ -3,6 +3,7 @@ import {
   Col,
   Row,
 } from 'react-bootstrap';
+import Moment from 'moment';
 import { getComments } from '../../api/comments';
 import styles from './styles.module.scss';
 
@@ -10,65 +11,119 @@ const ForumPosts:FC = () => {
   const { comments } = getComments();
 
   return (
-    <Col lg={{ span: 10, offset: 1 }}>
-      <div id={styles.centreConsole}>
-        <div id={styles.title}>Project Forum</div>
-        <Row>
-          <Col>
-            <div id={styles.heading1}>Key Info</div>
-          </Col>
-        </Row>
-        {
-          comments
-          && comments.map((comment :any) => {
-            if (comment.project.isClosed === false) {
+    <>
+      <Col lg={{ span: 10, offset: 1 }}>
+        <div id={styles.centreConsole}>
+          {
+            comments
+            && comments.map((comment :any) => {
+              if (comment.project.isClosed === false) {
+                return (
+                  <div key={comment.project.projectId}>
+                    <Row>
+                      <div className={styles.titleContainer}>
+                        <div id={styles.title}>{comment.project.projectName}</div>
+                      </div>
+                    </Row>
+                    <Row>
+                      <Col xs={1} md={1}>
+                        <div className={styles.firstInfo}>
+                          {comment.comment.userId}
+                        </div>
+                      </Col>
+                      <Col xs={2} md={2}>
+                        <div className={styles.firstInfo}>
+                          {Moment(comment.comment.createdAt).format(' Do MMM YYYY')}
+                        </div>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <div className={styles.projectStatus}>
+                          Open for further replies.
+                        </div>
+                      </Col>
+                    </Row>
+                    <div className={styles.projectIdea}>
+                      <Row>
+                        <Col xs={3} md={3}>
+                          <div className={styles.infoContainer}>
+                            <div className={styles.keyInfo}>{comment.project.projectName}</div>
+                          </div>
+                        </Col>
+                        <Col xs={2} md={2}>
+                          <div className={styles.infoContainer}>
+                            <div className={styles.keyInfo}>
+                              {comment.comment.userId}
+                            </div>
+                          </div>
+                        </Col>
+                        <Col xs={2} md={2}>
+                          <div className={styles.infoContainer}>
+                            <div className={styles.keyInfo}>
+                              Open
+                            </div>
+                          </div>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col xs={2} md={3}>
+                          <div className={styles.projDesc}>Project Description</div>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <div>
+                            {comment.comment.commentText}
+                          </div>
+                        </Col>
+                      </Row>
+                    </div>
+                  </div>
+                );
+              }
               return (
                 <div className={styles.projectIdea} key={comment.project.projectId}>
                   <Row>
-                    <Col xs={5} md={6}>
-                      <span className={styles.keyInfo}>{comment.project.projectName}</span>
-                    </Col>
-                    <Col xs={4} md={3}>
-                      <div className={styles.keyInfo}>
-                        Created by:
-                        {' '}
-                        {comment.comment.userId}
+                    <Col xs={3} md={3}>
+                      <div className={styles.infoContainer}>
+                        <div className={styles.keyInfo}>{comment.project.projectName}</div>
                       </div>
                     </Col>
-                    <Col xs={3} md={3}>
-                      <div className={styles.keyInfo}>
-                        Project Status: Open
+                    <Col xs={2} md={2}>
+                      <div className={styles.infoContainer}>
+                        <div className={styles.keyInfo}>
+                          {comment.comment.userId}
+                        </div>
+                      </div>
+                    </Col>
+                    <Col xs={2} md={2}>
+                      <div className={styles.infoContainer}>
+                        <div className={styles.keyInfo}>
+                          Closed
+                        </div>
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs={2} md={3}>
+                      <div className={styles.projDesc}>Project Description</div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <div>
+                        {comment.comment.commentText}
                       </div>
                     </Col>
                   </Row>
                 </div>
               );
-            }
-            return (
-              <div className={styles.projectIdea} key={comment.project.projectId}>
-                <Row>
-                  <Col xs={5} md={6}>
-                    <span className={styles.keyInfo}>{comment.project.projectName}</span>
-                  </Col>
-                  <Col xs={4} md={3}>
-                    <div className={styles.keyInfo}>
-                      Created by:
-                      {' '}
-                      {comment.comment.userId}
-                    </div>
-                  </Col>
-                  <Col xs={3} md={3}>
-                    <div className={styles.keyInfo}>
-                      Project Status: Closed
-                    </div>
-                  </Col>
-                </Row>
-              </div>
-            );
-          })
-        }
-      </div>
-    </Col>
+            })
+          }
+        </div>
+      </Col>
+    </>
   );
 };
 
