a8aaf19dc649b12ddb9334fa275260209bda7a98
Faraz <farazl786@hotmail.com>
2022-01-13 14:21:43 +0000

    Created login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,11 @@ import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
-import RegistrationForm from '../components/RegistrationForm';
-import LoginForm from '../components/LoginForm';
 
 const Application:FC<AppProps> = ({ Component, pageProps }) => (
   <>
     <Navigation />
     <Component {...pageProps} />
-    <RegistrationForm />
-    <LoginForm />
     <Footer />
   </>
 );
