4d459e56e52c1e1a1c0afb6400a8cb2ad1f9758d
acwatts <acwatts.dev@gmail.com>
2022-01-13 13:54:00 +0000

    Made initial values for group registration of type group

--- components/GroupRegistrationForm/index.tsx
+++ components/GroupRegistrationForm/index.tsx
@@ -10,53 +10,59 @@ import {
 } from 'react-bootstrap';
 import styles from './styles.module.scss';
 import UserSearch from '../UserSearch';
+import postGroup from '../../api/groups';
+import Group from '../../types/group';
 
-const GroupRegistrationForm: FC = () => (
-  <Container>
-    <h2 className={styles.formHeading}>Group Registration</h2>
-    <Formik
-      initialValues={{
-        groupName: '',
-        groupDescription: '',
-        groupMembers: [],
-        files: [],
-      }}
-      onSubmit={() => undefined}
-      validationSchema={
-        object({
-          groupName: string().min(5).max(30).required(),
-          groupDescription: string().min(10).max(100).required(),
-          groupMembers: array().min(2).max(4).required(),
-          groupFiles: array(),
-        })
-      }
-    >
-      {({ touched, errors }) => (
-        <Form>
-          <FormGroup controlId="groupName" className={styles.formGroup}>
-            {(touched.groupName && errors.groupName) ? <Alert>{errors.groupName}</Alert> : ''}
-            <FormLabel>Group Name</FormLabel>
-            <Field type="text" name="groupName" as={FormControl} placeholder="Amazing group" />
-          </FormGroup>
+const GroupRegistrationForm: FC = () => {
+  const initialValues: Group = {
+    groupName: '',
+    groupDescription: '',
+    groupMembers: [],
+    files: [],
+  };
 
-          <FormGroup controlId="groupDescription" className={styles.formGroup}>
-            {(touched.groupDescription && errors.groupDescription) ? <Alert>{errors.groupDescription}</Alert> : ''}
-            <FormLabel>Group Description</FormLabel>
-            <Field type="text" name="groupDescription" as={FormControl} placeholder="A truly amazing group" />
-          </FormGroup>
+  return (
+    <Container>
+      <h2 className={styles.formHeading}>Group Registration</h2>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => postGroup(values)}
+        validationSchema={
+          object({
+            groupName: string().min(5).max(30).required(),
+            groupDescription: string().min(10).max(100).required(),
+            groupMembers: array().min(2).max(4).required(),
+            groupFiles: array(),
+          })
+        }
+      >
+        {({ touched, errors }) => (
+          <Form>
+            <FormGroup controlId="groupName" className={styles.formGroup}>
+              {(touched.groupName && errors.groupName) ? <Alert>{errors.groupName}</Alert> : ''}
+              <FormLabel>Group Name</FormLabel>
+              <Field type="text" name="groupName" as={FormControl} placeholder="Amazing group" />
+            </FormGroup>
 
-          <UserSearch />
+            <FormGroup controlId="groupDescription" className={styles.formGroup}>
+              {(touched.groupDescription && errors.groupDescription) ? <Alert>{errors.groupDescription}</Alert> : ''}
+              <FormLabel>Group Description</FormLabel>
+              <Field type="text" name="groupDescription" as={FormControl} placeholder="A truly amazing group" />
+            </FormGroup>
 
-          <button
-            type="submit"
-            className="p-2 bg-blue-900 text-white my-2 rounded-sm"
-          >
-            Create Group
-          </button>
-        </Form>
-      )}
-    </Formik>
-  </Container>
-);
+            <UserSearch />
+
+            <button
+              type="submit"
+              className="p-2 bg-blue-900 text-white my-2 rounded-sm"
+            >
+              Create Group
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
+};
 
 export default GroupRegistrationForm;
