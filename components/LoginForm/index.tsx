45d1c8c6fa8beeafb29ef8a3596ae32d964ca83c
acwatts <acwatts.dev@gmail.com>
2022-02-02 19:24:08 +0000

    Fixed merge conflict

--- components/LoginForm/index.tsx
+++ components/LoginForm/index.tsx
@@ -28,32 +28,6 @@ const LoginForm:FC = () => {
         <>
           <h1>Login</h1>
           <Form>
-<<<<<<< HEAD
-            <Row>
-              <Col lg={{ span: 10, offset: 1 }}>
-                <FormGroup controlId="email">
-                  <FormLabel>Kent Email</FormLabel>
-                  <Field name="email" type="email" placeholder="example@kent.ac.uk" as={FormControl} />
-                </FormGroup>
-              </Col>
-            </Row>
-            <div className={styles.formspacer} />
-            <Row>
-              <Col lg={{ span: 10, offset: 1 }}>
-                <FormGroup controlId="password">
-                  <FormLabel>Password</FormLabel>
-                  <Field name="password" type="password" placeholder="password" as={FormControl} />
-                </FormGroup>
-              </Col>
-            </Row>
-            <div className={styles.formspacer} />
-            <div className={styles.formspacer} />
-            <Row>
-              <Col className={styles.buttonWrapper}>
-                <button type="submit" className={styles.save}>Login</button>
-              </Col>
-            </Row>
-=======
             <div className="my-3">
               <Label htmlFor="email">Email</Label>
               <Input name="email" id="email" type="email" placeholder="example@kent.ac.uk" />
@@ -63,7 +37,6 @@ const LoginForm:FC = () => {
               <Input name="password" id="password" type="password" placeholder="Password123" />
             </div>
             <button type="submit" className="px-3 py-2 upper bg-emerald-600 text-white rounded">Login</button>
->>>>>>> 7d54055d1a21e1f11e1c12df3fd5969f2898911a
           </Form>
         </>
       </Formik>
