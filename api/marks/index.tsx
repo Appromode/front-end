f6d4685f09f15d9749fc687c7e1866a98b2632b8
Alexander Watts <alexanderwatts@stu85FA.kent.ac.uk>
2022-02-23 22:36:26 +0000

    Updated marks in api to be grade

--- api/marks/index.tsx
+++
@@ -1,9 +0,0 @@
-import poster from '../../utils/poster';
-import fetcher from '../../utils/fetcher';
-import Marks from '../../types/marks';
-
-export const getMark = () => fetcher('/api/Mark');
-
-export const postMark = (marks: Marks) => poster('/api/Mark', 'POST', marks);
-
-export const putMark = (marks: Marks) => poster('/api/Mark', 'PUT', marks);
