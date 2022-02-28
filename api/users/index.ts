65436052506d275f69fcc30de6ebdd289a2c9097
Faraz <farazl786@hotmail.com>
2022-02-28 12:12:19 +0000

    Created put method for users

--- api/users/index.ts
+++ api/users/index.ts
@@ -1,6 +1,9 @@
 import useSWR from 'swr';
 import User from '../../types/user';
 import fetcher from '../../utils/fetcher';
+import poster from '../../utils/poster';
+
+export const putUser = (user: User) => poster('/api/User', 'PUT', user);
 
 export const getUsers = () => {
   const { data, error } = useSWR('/api/User', fetcher);
