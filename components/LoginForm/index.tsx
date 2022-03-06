992fb8dcdffe29cca8a4960032818ae8a3ac4ff6
Alexander Watts <alexanderwatts@stu845B.kent.ac.uk>
2022-03-06 04:07:48 +0000

    Added router and post to api login

--- components/LoginForm/index.tsx
+++ components/LoginForm/index.tsx
@@ -1,14 +1,18 @@
 import React, { FC, useContext } from 'react';
+import { useRouter } from 'next/router';
 import { Formik, Form } from 'formik';
 import { object, SchemaOf, string } from 'yup';
 import { postUser } from '../../api/users';
-import { AuthContext } from '../../stores/AuthContext';
 import Input from '../Input';
 import Label from '../Label';
 import Login from '../../types/login';
+import { localPoster } from '../../utils/poster';
+import UserToken from '../../types/user-token';
+import { AuthContext } from '../../stores/AuthContext';
 
 const LoginForm:FC = () => {
-  const { setUser } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
+  const router = useRouter();
 
   const initialValues: Login = {
     email: '',
@@ -27,7 +31,8 @@ const LoginForm:FC = () => {
         onSubmit={(values, { setSubmitting, setStatus }) => {
           setSubmitting(true);
           postUser(values)
-            .then((data) => setUser(data))
+            .then((data) => localPoster<UserToken>('/api/login', 'POST', data))
+            .then(() => router.push(`/dashboard/${user.nameid}`))
             .catch(() => setStatus('There was an issue, try again later'));
         }}
         validationSchema={validationSchema}
