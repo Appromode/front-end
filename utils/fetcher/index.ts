69e3a759e3ba237043874ecd0bec2898f0adfff4
Alexander Watts <alexanderwatts@eduD9E5.kent.ac.uk>
2022-03-19 12:00:43 +0000

    Updated fetcher and poster fix

--- utils/fetcher/index.ts
+++ utils/fetcher/index.ts
@@ -5,9 +5,7 @@ const fetcher = async (url: string) => {
 
   return fetch(`${process.env.NEXT_PUBLIC_API_ROUTE}${url}`, {
     method: 'GET',
-    credentials: 'include',
     headers: {
-      'Access-Control-Allow-Credentials': '*',
       Authorization: `Bearer ${authorization}`,
     },
   }).then((data) => data.json());
--- utils/poster/index.ts
+++ utils/poster/index.ts
@@ -1,14 +1,18 @@
 type Method = 'PUT' | 'PATCH' | 'POST';
 
-const poster = <T>(url: string, method: Method, data: object | string, remote: boolean = true):
+const poster = async<T>(url: string, method: Method, data: object | string, remote: boolean = true):
   Promise<T> => {
   const URI = remote ? `${process.env.NEXT_PUBLIC_API_ROUTE}${url}` : url;
 
+  const res = await fetch('/api/token');
+
+  const authorization = await res.text();
+
   return fetch(URI, {
-    method: 'POST',
-    credentials: 'include',
+    method,
     headers: {
       'Content-Type': 'application/json;charset=UTF-8',
+      Authorization: `Bearer ${authorization}`,
     },
     body: JSON.stringify(data),
   }).then((response) => response.json());
