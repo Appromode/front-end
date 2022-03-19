26937d17f248ff64010e9850e6ce0cefa5e19ece
Alexander Watts <alexanderwatts@eduD9E5.kent.ac.uk>
2022-03-19 11:34:52 +0000

    Updated new dashboard page

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 import { Container, Row } from 'react-bootstrap';
+import PasswordForm from '../../components/PasswordForm';
+import PublicSettingsForm from '../../components/PublicSettingsForm';
 import withAuthorization from '../../utils/withAuthorization';
 
 const Dashboard:FC = () => (
@@ -8,6 +10,20 @@ const Dashboard:FC = () => (
       <Container>
         <Row>
           <h1 className="text-3xl">Dashboard</h1>
+        </Row>
+      </Container>
+    </div>
+    <div className="py-20 bg-white">
+      <Container>
+        <Row>
+          <PublicSettingsForm />
+        </Row>
+      </Container>
+    </div>
+    <div className="py-20 bg-white">
+      <Container>
+        <Row>
+          <PasswordForm />
         </Row>
       </Container>
     </div>
