5fe5a337adc2cbd7f5b03168b0937551092461fb
Brad <bss25@kent.ac.uk>
2022-03-21 13:25:33 +0000

    Revert "Merge pull request #61 from Appromode/table-pagination"
    This reverts commit 3590f9ee885e439770df34eea4a1c56f72bb919d, reversing
    changes made to 93714a0c70626757131a912f4fb427b45e68bd71.

--- utils/poster/index.ts
+++ utils/poster/index.ts
@@ -12,11 +12,7 @@ const poster = async<T>(url: string, method: Method, data: object | string, remo
     method,
     headers: {
       'Content-Type': 'application/json;charset=UTF-8',
-<<<<<<< HEAD
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI4MGZkYjRhOC05OTRjLTExZWMtYjU3NS1iNjZiZjU4Y2RkYWYiLCJlbWFpbCI6InNqcDc1QGtlbnQuYWMudWsiLCJqdGkiOiI5ODk1MDE4Ni0xMDQzLTQwYzctYjI2My05MmRhMjZhNTdiNzQiLCJuYmYiOjE2NDYyNTkzMjUsImV4cCI6MTY2MjE1MzMyNSwiaWF0IjoxNjQ2MjU5MzI1fQ.1ZBhiwGWz7GEy203V_GcCtvKUtdAtDU2xHWrO7Oz780',
-=======
       Authorization: `Bearer ${authorization}`,
->>>>>>> develop
     },
     body: JSON.stringify(data),
   }).then((response) => response.json());
