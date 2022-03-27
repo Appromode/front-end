75362036a4f92206c79de874d90d8accc92cf340
Alexander Watts <alexanderwatts@eduD9E5.kent.ac.uk>
2022-03-27 14:59:59 +0100

    Removed has mounted from user groups component

--- components/UserGroup/index.tsx
+++ components/UserGroup/index.tsx
@@ -1,17 +1,13 @@
 import React, { useContext } from 'react';
 import { getGroup } from '../../api/users';
 import AuthContext from '../../stores/AuthContext';
-import useHasMounted from '../../utils/useHasMounted';
 
 const UserGroup = () => {
-  const hasMounted = useHasMounted();
   const { user } = useContext(AuthContext);
 
   const { group } = getGroup(user.nameid);
 
-  console.log('Group', group);
-
-  return hasMounted && (
+  return (
     <>
       <h2 className="text-3xl mb-5">Group</h2>
       {
