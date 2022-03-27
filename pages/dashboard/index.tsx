c46766bf0f5be6652600c9eda985b49de93f3115
Alexander Watts <alexanderwatts@eduD9E5.kent.ac.uk>
2022-03-27 15:00:44 +0100

    Added invites and user group to users dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react';
 import { Container, Row } from 'react-bootstrap';
+import InvitesTable from '../../components/InvitesTable';
+import UserGroup from '../../components/UserGroup';
 import withAuthorization from '../../utils/withAuthorization';
 
 const Dashboard:FC = () => (
-  <>
+  <div className="divide-gray-400 divide">
     <div className="py-20 bg-bottle text-white">
       <Container>
         <Row>
@@ -11,7 +13,17 @@ const Dashboard:FC = () => (
         </Row>
       </Container>
     </div>
-  </>
+    <div className="py-20">
+      <Container>
+        <UserGroup />
+      </Container>
+    </div>
+    <div className="py-20">
+      <Container>
+        <InvitesTable />
+      </Container>
+    </div>
+  </div>
 );
 
 export default withAuthorization(Dashboard);
