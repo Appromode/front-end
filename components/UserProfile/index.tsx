633219317418b62bdb62bc697756d1de57da96f9
Alexander Watts <alexanderwatts@eduD9E5.kent.ac.uk>
2022-03-29 23:23:10 +0100

    Updated user profile

--- components/UserProfile/index.tsx
+++ components/UserProfile/index.tsx
@@ -1,16 +1,19 @@
+import { useRouter } from 'next/router';
 import React, { FC } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { getUser } from '../../api/users';
 
 const UserProfile: FC = () => {
-  const { user } = getUser('');
+  const router = useRouter();
+
+  const { user } = getUser(router.asPath.slice(7));
 
   return (
     <div className="p-20 bg-bottle text-white">
       <Container>
         <Row>
           <Col>
-            <h1 className="text-3xl">User</h1>
+            <h1 className="text-3xl">{user?.normalizedEmail}</h1>
           </Col>
         </Row>
       </Container>
